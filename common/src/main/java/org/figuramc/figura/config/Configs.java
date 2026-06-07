483991657cbd99b3a20c8449c9781ef35ac63fb0
Eythrocyte3803 <2544390577@qq.com>
2026-06-07 15:59:30 +0900

    修复 Figura 和 YSM(以及各种分支) 同时安装时候，第一人称视角被遮挡的问题

--- common/src/main/java/org/figuramc/figura/config/Configs.java
+++ common/src/main/java/org/figuramc/figura/config/Configs.java
@@ -18,6 +18,7 @@ import org.figuramc.figura.permissions.Permissions;
 import org.figuramc.figura.resources.FiguraRuntimeResources;
 import org.figuramc.figura.utils.ColorUtils;
 import org.figuramc.figura.utils.FiguraText;
+import org.figuramc.figura.utils.PlatformUtils;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -140,7 +141,7 @@ public class Configs {
             }};
     public static final ConfigType.BoolConfig
             ALLOW_FP_HANDS = new ConfigType.BoolConfig("allow_fp_hands", RENDERING, false),
-            FIRST_PERSON_MATRICES = new ConfigType.BoolConfig("first_person_matrices", RENDERING, true),
+            FIRST_PERSON_MATRICES = new ConfigType.BoolConfig("first_person_matrices", RENDERING, !PlatformUtils.isModLoaded("yes_steve_model")),
             INVENTORY_SCISSOR = new ConfigType.BoolConfig("inventory_scissor", RENDERING, true),
             RENDER_STATS = new ConfigType.BoolConfig("render_stats", RENDERING, true, false);
 
